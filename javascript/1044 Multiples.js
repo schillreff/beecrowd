541dc5121b771e4778c6b818414f33e031593b98
Leandro Schillreff <leandroschillreff@gmail.com>
2025-02-01 09:48:13 -0400

    feat: solution to beecrowd problem

---
+++ javascript/1044 Multiples.js
@@ -0,0 +1,28 @@
+// Read two integer values (A and B). After, the program should print the
+//  message "Sao Multiplos" (are multiples) or "Nao sao Multiplos"
+// (aren’t multiples), corresponding to the read values.
+
+// Input
+// The input has two integer numbers.
+
+// Output
+// Print the relative message to the input as stated above.
+
+// Input Sample	Output Sample
+// 6 24
+
+// Sao Multiplos
+
+// 6 25
+
+// Nao sao Multiplos
+
+let input = require("fs").readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+let [A, B] = lines.shift().split(" ").map(Number);
+
+if (A > B) {
+  [A, B] = [B, A];
+}
+B % A === 0 ? console.log("Sao Multiplos") : console.log("Nao sao Multiplos");

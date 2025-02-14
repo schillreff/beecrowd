5a719e7b8f1d530f7e6b50bd4fa5dbdc43c7c005
Leandro Schillreff <leandroschillreff@gmail.com>
2025-02-14 10:10:19 -0400

    feat: solution to beecrowd problem

---
+++ python/1002 Area of a Circle.py
@@ -0,0 +1,34 @@
+# A fórmula para calcular a área de uma circunferência é: area = π . raio2. 
+# Considerando para este problema que π = 3.14159:
+
+# - Efetue o cálculo da área, elevando o valor de raio ao quadrado e 
+# multiplicando por π.
+
+# Entrada
+# A entrada contém um valor de ponto flutuante (dupla precisão), no caso,
+#  a variável raio.
+
+# Saída
+# Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme 
+# exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla
+#  precisão (double). Como todos os problemas, não esqueça de imprimir o fim de 
+# linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+# Exemplos de Entrada	Exemplos de Saída
+# 2.00                  # A=12.5664
+
+# 100.64                # A=31819.3103
+
+# 150.00                # A=70685.7750
+
+
+
+# Leitura da entrada no terminal
+raio = float(input())
+
+# Cálculo da área
+pi = 3.14159
+area = pi * raio**2
+
+# Saída formatada com 4 casas decimais
+print(f"A={area:.4f}")

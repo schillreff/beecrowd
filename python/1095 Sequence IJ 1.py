29b7f40648b5e318286b069a163633cf578affe8
Leandro Schillreff <leandroschillreff@gmail.com>
2025-02-26 06:30:34 -0400

    feat: solution to beecrowd problem

---
+++ python/1095 Sequence IJ 1.py
@@ -0,0 +1,22 @@
+# Make a program that prints the sequence like the following example.
+
+# Input
+# This problem doesn't have input.
+
+# Output
+# Print the sequence like the example below.
+
+# Input Sample	Output Sample
+# I=1 J=60
+# I=4 J=55
+# I=7 J=50
+# ...
+# I=? J=0
+
+I = 1
+J = 60
+
+while J >= 0:
+    print(f"I={I} J={J}")
+    I += 3
+    J -= 5

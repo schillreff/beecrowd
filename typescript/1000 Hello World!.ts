b1c13fd0c8b7f398a52f47b49b955bb557aa69e9
Leandro Schillreff <leandroschillreff@gmail.com>
2025-03-14 19:17:39 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1000 Hello World!.ts
@@ -0,0 +1,13 @@
+// Welcome to beecrowd!
+// Your first program in any programming language is usually
+// "Hello World!". In this first problem, all you need to do is print
+// this message on the screen.
+// Input
+// This problem has no input.
+// Output
+// You must print the message "Hello World!" followed by a new line,
+// as shown below.
+// Input Example   Output Example
+//                 Hello World!
+
+console.log("Hello World!");

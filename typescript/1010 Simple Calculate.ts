8372cbf9f5d54e5e1dab1c9aa94ab965d96d8af3
Leandro Schillreff <leandroschillreff@gmail.com>
2025-03-16 07:46:39 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1010 Simple Calculate.ts
@@ -0,0 +1,34 @@
+// Neste problema, deve-se ler o código de uma peça 1, o número de peças 1,
+// o valor unitário de cada peça 1, o código de uma peça 2, o número de peças
+//  2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+// Entrada
+// O arquivo de entrada contém duas linhas de dados. Em cada linha haverá
+//  3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+// Saída
+// A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo,
+//  lembrando de deixar um espaço após os dois pontos e um espaço após o "R$".
+//  O valor deverá ser apresentado com 2 casas após o ponto.
+
+// Exemplos de Entrada	Exemplos de Saída
+// 12 1 5.30
+// 16 2 5.10            // VALOR A PAGAR: R$ 15.50
+
+// 13 2 15.30
+// 161 4 5.20           // VALOR A PAGAR: R$ 51.40
+
+// 1 1 15.10
+// 2 1 15.10            // VALOR A PAGAR: R$ 30.20
+
+import * as fs from 'fs';
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const [code1, quantity1, price1] = lines[0].split(" ").map(Number);
+const [code2, quantity2, price2] = lines[1].split(" ").map(Number);
+
+const totalAmount = quantity1 * price1 + quantity2 * price2;
+
+console.log(`VALOR A PAGAR: R$ ${totalAmount.toFixed(2)}`);

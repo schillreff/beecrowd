fc4c04eda6a7395fa38b59c5777a48fcdfd5360d
Leandro Schillreff <leandroschillreff@gmail.com>
2025-03-28 17:59:01 -0400

    feat: solution to beecrowd problem

---
+++ javascript/1158 Sum of Consecutive Odd Numbers III.js
@@ -0,0 +1,40 @@
+// Read an integer N that is the number of test cases that follows. Each test
+// case contains two integers X and Y. Print one output line for each test case
+//  that is the sum of Y odd numbers from X including it if is the case. For example:
+// for the input 4 5, the output must be 45, that is: 5 + 7 + 9 + 11 + 13
+// for the input 7 4, the output must be 40, that is: 7 + 9 + 11 + 13
+
+// Input
+// The first line of the input is an integer N that is the number of test cases
+//  that follow. Each test case is a line containing two integer X and Y.
+
+// Output
+// Print the sum of all consecutive odd numbers from X.
+
+// Input Sample	Output Sample
+// 2
+// 4 3
+// 11 2
+
+// 21
+// 24
+
+let input = require("fs").readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const n = parseInt(lines.shift());
+for (let i = 1; i <= n; i++) {
+  let [x, y] = lines.shift().split(" ").map(Number);
+
+  if (x % 2 === 0) {
+    x++;
+  }
+
+  let total = 0;
+  for (let j = 0; j < y; j++) {
+    total += x;
+    x += 2;
+  }
+
+  console.log(total);
+}

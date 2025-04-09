f3c38258cec9fa33d6c933d4d4bf295e622be905
Leandro Schillreff <leandroschillreff@gmail.com>
2025-04-09 12:03:44 -0400

    feat: solution to beecrowd problem

---
+++ javascript/1175 Array change I.js
@@ -0,0 +1,35 @@
+// Write a program that reads an array N [20]. After, change the first element 
+// by the last, the second element by the last but one, etc.., Up to change the
+//  10th to the 11th. print the modified array.
+
+// Input
+// The input contains 20 integer numbers, positive or negative.
+
+// Output
+// For each position of the array N print "N[i] = Y", where i is the array position 
+// and Y is the number stored in that position.
+
+// Input Sample	Output Sample
+// 0
+// -5
+// ...
+// 63
+// 230
+
+// N[0] = 230
+// N[1] = 63
+// ...
+// N[18] = -5
+// N[19] = 0
+
+let input = require("fs").readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n").map(Number);
+let N = lines.slice(0, 20);
+
+for (let i = 0; i < 10; i++) {
+    [N[i], N[19 - i]] = [N[19 - i], N[i]];
+}
+
+for (let i = 0; i < 20; i++) {
+    console.log(`N[${i}] = ${N[i]}`);
+}

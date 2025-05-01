567194a448349acf7fe67f39d94706068b60fb76
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-01 08:16:32 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1049 Animal.ts
@@ -0,0 +1,61 @@
+// In this problem, your job is to read three Portuguese words. These words
+// define an animal according to the table below, from left to right. After,
+//  print the chosen animal defined by these three words.
+
+// Input
+// The input contains 3 words, one by line, that will be used to identify
+// the animal, according to the above table, with all letters in lowercase.
+
+// Output
+// Print the animal name according to the given input.
+
+// Input Samples	Output Samples
+// vertebrado
+// mamifero
+// onivoro
+
+// homem
+
+// vertebrado
+// ave
+// carnivoro
+
+// aguia
+
+// invertebrado
+// anelideo
+// onivoro
+
+// minhoca
+
+import * as fs from 'fs';
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const [word1, word2, word3] = lines;
+
+const animals = {
+  vertebrado: {
+    ave: {
+      carnivoro: "aguia",
+      onivoro: "pomba",
+    },
+    mamifero: {
+      onivoro: "homem",
+      herbivoro: "vaca",
+    },
+  },
+  invertebrado: {
+    inseto: {
+      hematofago: "pulga",
+      herbivoro: "lagarta",
+    },
+    anelideo: {
+      hematofago: "sanguessuga",
+      onivoro: "minhoca",
+    },
+  },
+};
+
+console.log(animals[word1][word2][word3]);

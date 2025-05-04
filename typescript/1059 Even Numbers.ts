c2d256de0337d31e514f0a659876f11f0f95c1a3
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-04 08:45:37 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1059 Even Numbers.ts
@@ -0,0 +1,18 @@
+// Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+
+// Input
+// In this extremely simple problem there is no input.
+
+// Output
+// Print all even numbers between 1 and 100, including them, one by row.
+
+// Input Sample	Output Sample
+// 2
+// 4
+// 6
+// ...
+// 100
+
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i);
+}

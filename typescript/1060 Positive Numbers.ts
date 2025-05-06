8c36dc12a68d2b148e84d09dd4fa8bef900aead0
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-06 15:05:20 -0400

    feat: add solution for counting positive numbers

---
+++ typescript/1060 Positive Numbers.ts
@@ -0,0 +1,28 @@
+// Write a program that reads 6 numbers. These numbers will only be positive or
+//  negative (disregard null values). Print the total number of positive numbers.
+
+// Input
+// Six numbers, positive and/or negative.
+
+// Output
+// Print a message with the total number of positive numbers.
+
+// Input Sample	Output Sample
+// 7
+// -5
+// 6
+// -3.4
+// 4.6
+// 12
+
+// 4 valores positivos
+
+import * as fs from 'fs';
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const result = lines.map(Number).reduce((count, num) => {
+  return num > 0 ? count + 1 : count;
+}, 0);
+console.log(`${result} valores positivos`);

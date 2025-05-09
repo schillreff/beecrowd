0193bd1545ae681f444ced8ce043be092d6ec31b
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-09 14:52:18 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1065 Even Between five Numbers.ts
@@ -0,0 +1,32 @@
+// Make a program that reads five integer values. Count how many of these
+// values ​​are even and  print this information like the following example.
+
+// Input
+// The input will be 5 integer values.
+
+// Output
+// Print a message like the following example with all letters in lowercase,
+// indicating how many even numbers were typed.
+
+// Input Sample	Output Sample
+// 7
+// -5
+// 6
+// -4
+// 12
+
+// 3 valores pares
+
+import * as fs from "fs";
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+let evenCount = 0;
+for (let i = 0; i < 5; i++) {
+  let value = parseInt(lines.shift() || "");
+  if (value % 2 === 0) {
+    evenCount++;
+  }
+}
+console.log(`${evenCount} valores pares`);

ef69a83dd458677d536cbac8493525ea915c1ad3
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-10 17:47:42 -0400

    feat: solution to beecrowd problem

---
+++ java/1000 Hello World!.java
@@ -0,0 +1,19 @@
+// Welcome to beecrowd!
+// Your first program in any programming language is usually
+// "Hello World!". In this first problem, all you need to do is print
+// this message on the screen.
+// Input
+// This problem has no input.
+// Output
+// You must print the message "Hello World!" followed by a new line,
+// as shown below.
+// Input Example   Output Example
+//                 Hello World!
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}

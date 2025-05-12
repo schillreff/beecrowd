f1d23c8661faf5c630b64f6bdec0f3d0c17b4dba
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-12 15:47:08 -0400

    feat: solution to beecrowd problem

---
+++ java/1015 Distance Between Two Points.java
@@ -0,0 +1,45 @@
+// Read the four values corresponding to the x and y axes of two points in
+// the plane, p1 (x1, y1) and p2 (x2, y2) and calculate the distance between
+// them, showing four decimal places, according to the formula:
+
+// Distance =
+
+// Input
+// The input file contains two lines of data. The first one contains two double
+//  values: x1 y1 and the second one also contains two double values with one
+//  digit after the decimal point: x2 y2.
+
+// Output
+// Calculate and print the distance value using the provided formula, with
+// 4 decimal places.
+
+// Input Sample	Output Sample
+// 1.0 7.0
+// 5.0 9.0      // 4.4721
+
+// -2.5 0.4
+// 12.1 7.3     // 16.1484
+
+// 2.5 -0.4
+// -12.2 7.0    // 16.4575
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println(String.format("%.4f", distance));
+
+        scanner.close();
+    }
+}

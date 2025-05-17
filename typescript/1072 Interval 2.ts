52f8f79be83124a95036352f02c22a1372bbb256
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-17 13:07:18 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1072 Interval 2.ts
@@ -0,0 +1,42 @@
+// Read an integer N. This N will be the number of integer numbers X that will be read.
+
+// Print how many these numbers X are in the interval [10,20] and how many values are out of this interval.
+
+// Input
+// The first line of input is an integer N (N < 10000), that indicates the total number of test cases.
+// Each case is an integer number X (-107 < X < 107).
+
+// Output
+// For each test case, print how many numbers are in and how many values are out of the interval.
+
+// Input Sample	Output Sample
+// 4
+// 14
+// 123
+// 10
+// -25
+
+// 2 in
+// 2 out
+
+import * as fs from "fs";
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const n = Number(lines.shift());
+
+let inInterval = 0,
+  outInterval = 0;
+
+for (let i = 0; i < n; i++) {
+  let x = Number(lines.shift());
+  if (x >= 10 && x <= 20) {
+    inInterval += 1;
+  } else {
+    outInterval += 1;
+  }
+}
+
+console.log(`${inInterval} in`);
+console.log(`${outInterval} out`);

30cffa475da9dfa3eef561cab4e9d260c19aa54b
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-17 13:07:33 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1078 Multiplication Table.ts
@@ -0,0 +1,33 @@
+// Read an integer N (2 < N < 1000). Print the multiplication table of N.
+// 1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+// Input
+// The input is an integer N (1 < N < 1000).
+
+// Output
+// Print the multiplication table of N., like the following example.
+
+// Input Sample	Output Sample
+// 140
+
+// 1 x 140 = 140
+// 2 x 140 = 280
+// 3 x 140 = 420
+// 4 x 140 = 560
+// 5 x 140 = 700
+// 6 x 140 = 840
+// 7 x 140 = 980
+// 8 x 140 = 1120
+// 9 x 140 = 1260
+// 10 x 140 = 1400
+
+import * as fs from 'fs';
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+const n = Number(lines.shift());
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} x ${n} = ${i * n}`);
+}

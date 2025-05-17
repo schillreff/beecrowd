9fc1c89d5cf67499983ee380caec1148190c6b27
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-17 13:06:50 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1071 Sum of Consecutive Odd Numbers I.ts
@@ -0,0 +1,42 @@
+// Read two integer values X and Y. Print the sum of all odd values between them.
+
+// Input
+// The input file contain two integer values.
+
+// Output
+// The program must print an integer number. This number is the sum off all odd
+// values between both input values that must fit in an integer number.
+
+// Sample Input	Sample Output
+// 6
+// -5
+
+// 5
+
+// 15
+// 12
+
+// 13
+
+// 12
+// 12
+
+// 0
+
+import * as fs from 'fs';
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let [x, y] = input.split("\n").map(Number);
+
+const start = Math.min(x, y) + 1;
+const end = Math.max(x, y);
+
+let sumOdd = 0;
+
+for (let i = start; i < end; i++) {
+  if (i % 2 !== 0) {
+    sumOdd += i;
+  }
+}
+
+console.log(sumOdd);

70c179b9f815609bf650e6ffda7491c453dac908
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-22 08:11:23 -0400

    feat: solution to beecrowd problem

---
+++ java/1059 Even Numbers.java
@@ -0,0 +1,26 @@
+// Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+
+// Input
+// In this extremely simple problem there is no input.
+
+// Output
+// Print all even numbers between 1 and 100, including them, one by row.
+
+// Input Sample	Output Sample
+// 2
+// 4
+// 6
+// ...
+// 100
+
+// let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+// let lines = input.split('\n');
+
+public class Main {
+
+  public static void main(String[] args) {
+    for (int i = 2; i <= 100; i += 2) {
+      System.out.println(i);
+    }
+  }
+}

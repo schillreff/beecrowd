e9abc40fdee1f90efe727bb41db28bb82747b931
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-01 15:31:50 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1115 Quadrant.ts
@@ -0,0 +1,48 @@
+// Write a program to read the coordinates (X, Y) of an indeterminate number of
+// points in Cartesian system. For each point write the quadrant to which it belongs.
+//  The program finish when at least one of two coordinates is NULL
+//  (in this situation without writing any message).
+
+// Input
+// The input contains several tests cases. Each test case contains two integer numbers.
+
+// Output
+// For each test case, print the corresponding quadrant which these
+// coordinates belong, as in the example.
+
+// Input Sample	Output Sample
+// 2 2
+// 3 -2
+// -8 -1
+// -7 1
+// 0 2
+
+// primeiro
+// quarto
+// terceiro
+// segundo
+
+import * as fs from "fs";
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+while (true) {
+  const line = lines.shift();
+  if (!line) continue;
+  const [x, y] = line.split(" ").map(Number);
+
+  if (x === 0 || y === 0) {
+    break;
+  }
+
+  if (x > 0 && y > 0) {
+    console.log("primeiro");
+  } else if (x < 0 && y > 0) {
+    console.log("segundo");
+  } else if (x < 0 && y < 0) {
+    console.log("terceiro");
+  } else if (x > 0 && y < 0) {
+    console.log("quarto");
+  }
+}

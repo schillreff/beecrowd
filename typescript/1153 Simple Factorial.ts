993afd4abb1c391a24fb092b1cdd9b2582885be3
Leandro Schillreff <leandroschillreff@gmail.com>
2025-08-11 18:36:21 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1153 Simple Factorial.ts
@@ -0,0 +1,29 @@
+// Read a value N. Calculate and write its corresponding factorial.
+// Factorial of N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+
+// Input
+// The input contains an integer value N (0 < N < 13).
+
+// Output
+// The output contains an integer value corresponding to the factorial of N.
+
+// Input Sample	Output Sample
+// 4
+
+// 24
+
+import * as fs from "fs";
+
+let input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+const n = Number(lines.shift());
+console.log(factorial(n));

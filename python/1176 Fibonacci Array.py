50d5145fabd9f0040b3a67685274787f7f362ce0
Leandro Schillreff <leandroschillreff@gmail.com>
2025-09-21 16:47:02 -0400

    feat: solution to beecrowd problem

---
+++ python/1176 Fibonacci Array.py
@@ -0,0 +1,31 @@
+# Write a program that reads a number and print the Fibonacci number corresponding
+# to this read number. Remember that the first elements of the Fibonacci series
+#  are 0 and 1 and each next term is the sum of the two preceding it. All the Fibonacci
+#  numbers calculated in this program must fit in a unsigned 64 bits number.
+
+# Input
+# The first line of the input contains a single integer T, indicating the number
+# of test cases. Each test case contains a single integer N (0 ≤ N ≤ 60), corresponding to the N-th term of the Fibonacci series.
+
+# Output
+# For each test case in the input, print the message "Fib(N) = X", where X is the N-th term of the Fibonacci series.
+
+# Input Sample	Output Sample
+# 3
+# 0
+# 4
+# 2
+
+# Fib(0) = 0
+# Fib(4) = 3
+# Fib(2) = 1
+
+t = int(input().strip())
+
+fib = [0, 1]
+for i in range(2, 61):
+    fib.append(fib[i - 1] + fib[i - 2])
+
+for _ in range(t):
+    N = int(input().strip())
+    print(f"Fib({N}) = {fib[N]}")

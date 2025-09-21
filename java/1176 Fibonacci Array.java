ec791b6834d1d418a61e58a904c7e57965a370a6
Leandro Schillreff <leandroschillreff@gmail.com>
2025-09-21 16:51:47 -0400

    feat: solution to beecrowd problem

---
+++ java/1176 Fibonacci Array.java
@@ -0,0 +1,49 @@
+// Write a program that reads a number and print the Fibonacci number corresponding 
+// to this read number. Remember that the first elements of the Fibonacci series are 0 
+// and 1 and each next term is the sum of the two preceding it. All the Fibonacci numbers 
+// calculated in this program must fit in a unsigned 64 bits number.
+
+// Input
+// The first line of the input contains a single integer T, indicating the number
+//  of test cases. Each test case contains a single integer N (0 ≤ N ≤ 60), corresponding to the N-th term of the Fibonacci series.
+
+// Output
+// For each test case in the input, print the message "Fib(N) = X", where X is the N-th term of the Fibonacci series.
+
+// Input Sample	Output Sample
+// 3
+// 0
+// 4
+// 2
+
+// Fib(0) = 0
+// Fib(4) = 3
+// Fib(2) = 1
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        long[] fibonacci = new long[61];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= 60; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        int testCases = scanner.nextInt();
+
+        for (int i = 0; i < testCases; i++) {
+            int n = scanner.nextInt();
+            System.out.println("Fib(" + n + ") = " + fibonacci[n]);
+        }
+
+        scanner.close();
+    }
+}

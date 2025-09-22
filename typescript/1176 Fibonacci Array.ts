efb7bcda2fedfe58075567664dcfe1d3d49030d2
Leandro Schillreff <leandroschillreff@gmail.com>
2025-09-22 11:42:29 -0400

    feat: solution to beecrowd problem

---
+++ typescript/1176 Fibonacci Array.ts
@@ -0,0 +1,38 @@
+// Write a program that reads a number and print the Fibonacci number corresponding 
+// to this read number. Remember that the first elements of the Fibonacci series are 0 
+// and 1 and each next term is the sum of the two preceding it. All the Fibonacci numbers 
+// calculated in this program must fit in a unsigned 64 bits number.
+
+// Input
+// The first line of the input contains a single integer T, indicating the number
+//  of test cases. Each test case contains a single integer N (0 ≤ N ≤ 60), corresponding to the N-th term of the Fibonacci series.
+
+// Output
+// For each test case in the input, print the message "Fib(N) = X", where X is the N-th term of the Fibonacci series.
+
+// Input Sample	Output Sample
+// 3
+// 0
+// 4
+// 2
+
+// Fib(0) = 0
+// Fib(4) = 3
+// Fib(2) = 1
+
+
+import * as fs from 'fs';
+const input = fs.readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+let T = parseInt(lines.shift() || '0');
+let fib = [0, 1];
+
+for (let i = 2; i <= 60; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+for (let i = 0; i < T; i++) {
+    let N = parseInt(lines.shift() || '0');
+    console.log(`Fib(${N}) = ${fib[N]}`);
+}

2bf6917b0f65006b844703471ff37eb58de7faa5
Leandro Schillreff <leandroschillreff@gmail.com>
2025-10-03 13:06:35 -0400

    feat: solution to beecrowd problem

---
+++ kotlin/1177 Array Fill II.kt
@@ -0,0 +1,35 @@
+// Write a program that reads a number T and fill a vector N[1000] with the numbers from 0 to T-1 repeated times, like as the example below.
+
+// Input
+// The input contains an integer number T (2 ≤ T ≤ 50).
+
+// Output
+// For each position of the array N, print "N[i] = x", where i is the array position and x is the number stored in that position.
+
+// Input Sample	Output Sample
+// 3
+
+// N[0] = 0
+// N[1] = 1
+// N[2] = 2
+// N[3] = 0
+// N[4] = 1
+// N[5] = 2
+// N[6] = 0
+// N[7] = 1
+// N[8] = 2
+// ...
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val t = reader.readLine().toInt()
+    if (t >= 2 && t <= 50) {
+        for (i in 0 until 1000) {
+            val remainder = i % t
+            println("N[$i] = $remainder")
+        }
+    }
+}
